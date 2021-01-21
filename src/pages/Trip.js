8b1e26a98a7b1a44a936bce033db98a2ec29f081
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2021-01-22 00:21:38 +0700

    Update Trip.js

--- src/pages/Trip.js
+++ src/pages/Trip.js
@@ -10,11 +10,19 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Scrollbars } from 'react-custom-scrollbars';
-import {Typography,Paper,Tab,Tabs,Box,TextField,Select,InputLabel,MenuItem,FormControl,Fab,Card,CardContent,Divider} from "@material-ui/core";
+import {Typography,Button,Paper,Tab,Tabs,Box,TextField,Select,InputLabel,MenuItem,FormControl,Fab,IconButton,Card,CardContent,Divider,Avatar,Table,TableBody,TableCell,TableContainer,TableHead,TableRow,TableFooter,TablePagination} from "@material-ui/core";
 import PropTypes from 'prop-types';
 import placeOptions from '../data/PlaceData';
 import cityOptions from '../data/CityData';
 import modeOptions from '../data/modeOptions';
+import { withStyles, makeStyles,useTheme } from '@material-ui/core/styles';
+import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
 
 
 
@@ -313,7 +321,160 @@ function ChooseTabs() {
   );
 }
 
+//----------------- Table ----------------//
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    fontFamily: 'csPrajad',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  body: {
+    fontSize: 18,
+    fontFamily: 'csPrajad'
+  },
+}))(TableCell);
 
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData( tripName, dateAndTime, updateData ) {
+  return { tripName, dateAndTime, updateData };
+}
+const useStyles = makeStyles({
+  table: {
+    Width: 640,
+  },
+});
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+const rows = [
+  //ex data
+  createData('ชลบุรี', '30/01/2021 10:52AM'),
+  createData('ทริป', '31/01/2021 18:21PM'),
+  createData('ทริป101', '04/02/2021 01:23AM'),
+
+];
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function CustomizedTables() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage] = React.useState(5);
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ชื่อทริป</StyledTableCell>
+            <StyledTableCell align="center">วัน-เวลาที่สร้าง</StyledTableCell>
+            <StyledTableCell align="center">แก้ไข/ลบ</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.tripName}
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.dateAndTime}</StyledTableCell>
+              <StyledTableCell align="center">
+                <IconButton aria-label="edit" size="small"><EditRoundedIcon/></IconButton> /  <IconButton aria-label="delete" size="small"><DeleteForeverRoundedIcon/></IconButton>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+           <TablePagination
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              ActionsComponent={TablePaginationActions}
+            />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 {/*     ////////       main     ///////    */ }
 export class MapContainer extends Component {
@@ -331,63 +492,97 @@ export class MapContainer extends Component {
       </header>
 
       <section className="App-section">
-      <Container fluid style={{marginTop:50}}>
-        <Row style={{marginTop:5,marginLeft:95}} noGutters={true}>
-         
-        </Row>
+      <Container fluid style={{marginTop:50 , marginBottom:50}}>
         <Row>
-          <Col md="auto">
-            <Map
-          google={this.props.google}
-          zoom={9.5}
-          style={mapStyles}
-          disableDefaultUI ={true}
-          scrollwheel={false}
-          disableDoubleClickZoom = {true}
-          draggable={false}
-          zoomControl={false}
-          onReady={this.handleMapReady}
-          onBounds_changed={this.handleMapMount}
-          initialCenter={
-            {
-              lat:  13.12,
-              lng:  101.20
-            }
-          }
-            >
-              <Paper style={{width:875,height:575,marginLeft:57,marginTop:20}} elevation={5}/>
-            </Map>
-
-          </Col>
-          <Col style={{marginLeft:800}} md="auto">
-            <Jumbotron style={{backgroundSize:'cover' , backgroundColor:'#3C6E71', marginTop:20, marginLeft:150 , width:450}}>
-            <Card style={{marginTop:-20}}>
-                <CardContent >
-                  <Typography style={{fontFamily:"csPrajad" ,fontSize:24}}>
-                    แผนการท่องเที่ยว
-                  </Typography>
-                  <Typography color="textSecondary" style={{fontFamily:"csPrajad",fontSize:16}}>
-                    Trip Planner
-                  </Typography>
-                  <ChooseTabs/>
-                  <br />
-                  <Divider/>
-                </CardContent>
-              </Card>
-              <Fab
-                  variant="extended"
-                  size="large"
+          <Paper elevation={5} style={{width:1200,height:600,marginTop:15,marginLeft:140}}>
+            <Row>
+            <Col>
+            <Box p={5}>
+            <Card style={{backgroundColor:'#3C6E71',width:450}}>
+              <CardContent style={{color:'white'}}>
+              <Row style={{margin:5}}>
+                <Avatar alt="User" src="/" style={{width:60,height:60}}/>
+                <Typography style={{margin:10, fontFamily:"csPrajad" ,fontSize:24,fontWeight:'bold'}}>Profile</Typography>
+              </Row>
+              <Divider style={{margin:10}}/>
+              <Typography style={{fontFamily:"csPrajad" ,fontSize:18}}>ชื่อในระบบ :</Typography>
+              <Typography style={{fontFamily:"csPrajad" ,fontSize:18}}> E-mail :</Typography>
+              </CardContent>
+            </Card>
+            </Box>
+            <Box ml={14}>
+              <Button
+                  variant="contained"
                   color="secondary"
-                  aria-label="Add"
-                  style={{width:100,height:50,marginTop:10,marginLeft:280,marginBottom:-28}}
-              >
-                  Submit
-              </Fab>
-            </Jumbotron>
-              
-          </Col>
+                  style={{width:300,height:50}}
+              >My Trip</Button>
+            </Box>
+            <Box ml={14} pt={2}>
+              <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  style={{width:300,height:50}}
+              >Recommended Trip</Button>
+            </Box>
+            </Col>
+            <Col>
+            <Box pt={5} ml={-12}>
+            <Card style={{backgroundColor:'#284B63',width:640}}>
+              <CardContent style={{color:'white'}}>
+                <CustomizedTables/>
+                <Fab
+                    variant="extended"
+                    size="large"
+                    color="secondary"
+                    aria-label="Add"
+                    style={{width:150,height:60,margin:10}}
+                   >
+                      New Trip
+                  </Fab>
+              </CardContent>
+            </Card>
+            </Box>
+            </Col>
+            </Row>
+          </Paper> 
         </Row>
-        </Container>
+        <Box p={10}/>
+        <Row>
+          <Paper elevation={5} style={{width:400,height:600,marginTop:15,marginLeft:140}}>
+            <Card style={{backgroundColor:'#284B63',width:'auto'}}>
+              <CardContent style={{color:'white'}}>
+                <Typography style={{margin:10, fontFamily:"csPrajad" ,fontSize:20,fontWeight:'bold'}}>ข้อมูลทริป และเลือกที่พัก</Typography>
+              </CardContent>
+            </Card>
+            <Box p={2}>
+            <Typography style={{fontFamily:"csPrajad" ,fontSize:16,padding:5}}>ชื่อทริป :</Typography>
+            <Typography style={{fontFamily:"csPrajad" ,fontSize:16,padding:5}}>จังหวัด :</Typography>
+            <CityName/>
+            <br/>
+            <Typography style={{fontFamily:"csPrajad" ,fontSize:16,padding:5}}>วันที่ไป :</Typography>
+            <DateStart/>
+            <Typography style={{fontFamily:"csPrajad" ,fontSize:16,padding:5}}>วันที่กลับ :</Typography>
+            <DateEnd/>
+            <Typography style={{fontFamily:"csPrajad" ,fontSize:16,padding:5}}>ที่พัก :</Typography>
+            </Box>
+          </Paper>
+          <Paper elevation={5} style={{width:400,height:600,marginTop:15,marginLeft:20}}>
+            <Card style={{backgroundColor:'#284B63',width:'auto'}}>
+              <CardContent style={{color:'white'}}>
+              <Typography style={{margin:10, fontFamily:"csPrajad" ,fontSize:20,fontWeight:'bold'}}>จัดการทริป</Typography>
+              </CardContent>
+            </Card>
+          </Paper> 
+          <Paper elevation={5} style={{width:400,height:600,marginTop:15,marginLeft:20}}>
+            <Card style={{backgroundColor:'#284B63',width:'auto'}}>
+              <CardContent style={{color:'white'}}>
+              <Typography style={{margin:10, fontFamily:"csPrajad" ,fontSize:20,fontWeight:'bold'}}>แผนที่ทริป</Typography>
+              </CardContent>
+            </Card>
+          </Paper>  
+        </Row>  
+      </Container>
       </section>
     </div>
     );
