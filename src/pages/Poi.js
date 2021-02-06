5c4c5ebc4404f3622b768a8f65da6e3b4f66edda
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2021-02-07 02:35:23 +0700

    Update Poi.js

--- src/pages/Poi.js
+++ src/pages/Poi.js
@@ -9,12 +9,16 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import placeOptions from '../data/PlaceData';
 import cityOptions from '../data/CityData';
 import poiOptions from '../data/PoiData';
-import {Typography,Paper,TextField,Button,Select,InputLabel,MenuItem,FormControl,Fab,Card,CardContent,Divider,Box} from "@material-ui/core";
+import {Typography,Paper,TextField,Button,Select,InputLabel,MenuItem,FormControl,Fab,Card,List,ListItem,CardContent,Divider,Box,CardHeader} from "@material-ui/core";
 import Skeleton from '@material-ui/lab/Skeleton';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import RoomIcon from '@material-ui/icons/Room';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import TodayIcon from '@material-ui/icons/Today';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import CommuteIcon from '@material-ui/icons/Commute';
 //import axios from "axios";
 
 //Google Map
@@ -36,7 +40,7 @@ export function CityName() {
     getOptionLabel: (option) => option.cname_th +" ("+ option.cname_en +")",
   }
   return (
-    <div style={{ width:'25%' ,height:20,marginTop:-10}}>
+    <div style={{ width:'50%' ,height:20,marginTop:-10}}>
       <Autocomplete
         {...defaultProps}
         id="cityName"
@@ -54,7 +58,7 @@ export function PlaceName() {
     getOptionLabel: (option) => option.pname,
   }
   return (
-    <div style={{ width: '40%',marginTop:-10}}>
+    <div style={{ width: '60%',marginTop:-10}}>
       <Autocomplete
         {...defaultProps}
         id="placeName"
@@ -74,7 +78,7 @@ export function DateStart() {
       label="วันที่เริ่มต้น"
       type="date"
       required id="standard-required" 
-      style={{width:'100%',marginLeft:'2%'}}
+      style={{width:'100%',marginLeft:'2%',marginTop:'-6%'}}
       InputLabelProps={{
         shrink: true,
       }}
@@ -90,7 +94,7 @@ export function DateEnd() {
         label="วันที่สิ้นสุด"
         type="date"
         required id="standard-required" 
-        style={{ width:'100%',marginLeft:'2%'}}
+        style={{ width:'100%',marginLeft:'2%',marginTop:'-6%'}}
         InputLabelProps={{
           shrink: true,
         }}
@@ -228,105 +232,94 @@ export class MapContainer extends React.Component  {
       <section className="App-section">
 
       <Container fluid noGutters={true}>
-      <Paper elevation={5} style={{width:'90%',margin:'5%'}}>
-
-        <Row style={{margin:'1%'}} noGutters={true}>
-   
+      <Paper elevation={5} style={{width:'90%',height:'auto',margin:'5%'}}>
+        <Row noGutters>
+        <Col sm={5}>
+        <Card style={{height:'auto'}}>
+        <CardHeader style={{backgroundColor:'#284B63',color:'white',textAlign:'center'}} title="Tourism Statistics"/>
+        <CardContent>
             <InputGroup style={{marginTop:15}}>
-                <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",borderRadius:5}}>City</InputGroup.Text>
-                </InputGroup.Prepend>
+               <Typography style={{fontFamily:'csPrajad' ,fontSize:20,margin:'1%'}}>จังหวัด :</Typography>
                 <CityName/>
             </InputGroup>
- 
-        </Row>
-        <Row style={{margin:'1%'}} noGutters={true}>
-          
+
             <InputGroup style={{marginTop:15}}>
-                <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",borderRadius:5}}>Point of Interest</InputGroup.Text>
-                </InputGroup.Prepend>
+              <Typography style={{fontFamily:'csPrajad' ,fontSize:20,margin:'1%'}}>สถานที่ท่องเที่ยว :</Typography>
               <PlaceName/>
             </InputGroup>
-        </Row>
-        <Row style={{margin:'1%'}} noGutters={true}>
-          <InputGroup style={{marginTop:15}}>
-            <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",marginTop:10,marginRight:'5%',borderRadius:5}}>From</InputGroup.Text>
-                  <DateStart/>
-                  <StartTime/>
-              </InputGroup.Prepend>
-          </InputGroup>
-        </Row>
-        <Row style={{margin:'1%'}} noGutters={true}>
-          <Col>
-            <InputGroup style={{marginTop:15}}>
-                <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",marginTop:10,marginRight:'5%',borderRadius:5}}>To</InputGroup.Text>
-                  <DateEnd/>
-                  <EndTime/>          
-                </InputGroup.Prepend>
-            </InputGroup>
 
-            </Col>
-            <Col>
+          <InputGroup style={{marginTop:15}}>
+            <Typography style={{fontFamily:'csPrajad' ,fontSize:20,margin:'1%'}}>วันที่ :</Typography>
+              <DateStart/>
+          </InputGroup>
+
+          <InputGroup style={{marginTop:15}}>
+            <Typography style={{fontFamily:'csPrajad' ,fontSize:20,margin:'1%'}}>ถึงวันที่ :</Typography>
+              <DateEnd/>
+          </InputGroup>
+
               {/*Submit Button        */ }
               <Button
                     variant="contained"
                     size="large"
                     color="secondary"
-                    style={{width:'20%',height:60,margin:'2%',marginLeft:'75%',borderRadius:50}}
+                    style={{width:'20%',height:60,margin:'10%',marginLeft:'75%',borderRadius:50}}
               >
                 Submit
               </Button>
-            </Col>
-        </Row>
+
+        </CardContent>
+        </Card>
+        </Col>
+
+        <Col sm={7}>
+          <InputGroup style={{width:'100%',height:'90%'}}>
+            <Map
+                  google={this.props.google}
+                  zoom={9}
+                  disableDefaultUI ={true}
+                  scrollwheel={false}
+                  disableDoubleClickZoom = {true}
+                  draggable={false}
+                  zoomControl={false}
+                  onReady={this.handleMapReady}
+                  onBounds_changed={this.handleMapMount}
+                  initialCenter={
+                    {
+                      lat:  13.12,
+                      lng:  101.20
+                    }
+                  }
+                >      
+                {placeOptions.map(placeOptions=>(
+              <Marker key={placeOptions.pname} position={{lat:placeOptions.lat,lng:placeOptions.lon}} />
+            ))}
+            </Map>
+            </InputGroup>
+            <InputGroup style={{height:'10%'}}>
+            <Button
+                          variant="contained"
+                          size="large"
+                          color="secondary"
+                          style={{width:'100%'}}
+                          onClick={this.toggleHeatmap}
+                        >
+                            Open Heat Map
+            </Button>
+            </InputGroup>
+          </Col>
+          </Row>
         </Paper>
         
-        <Paper elevation={5} style={{width:'90%',height:'35%',margin:'5%',marginTop:'-4%'}}>
-          <InputGroup style={{width:'100%' ,height:'90%'}}>
-          <Map
-                google={this.props.google}
-                zoom={9}
-                style={{width:'100%',height:'auto'}}
-                disableDefaultUI ={true}
-                scrollwheel={false}
-                disableDoubleClickZoom = {true}
-                draggable={false}
-                zoomControl={false}
-                onReady={this.handleMapReady}
-                onBounds_changed={this.handleMapMount}
-                initialCenter={
-                  {
-                    lat:  13.12,
-                    lng:  101.20
-                  }
-                }
-              >      
-              {placeOptions.map(placeOptions=>(
-            <Marker key={placeOptions.pname} position={{lat:placeOptions.lat,lng:placeOptions.lon}} />
-          ))}
-     
-          </Map>
-          </InputGroup>
-          <Button
-                        variant="contained"
-                        size="large"
-                        color="secondary"
-                        style={{width:'100%',height:'10%'}}
-                        onClick={this.toggleHeatmap}
-                      >
-                          Open Heat Map
-                </Button>
-        </Paper>
-        <Paper elevation={5} style={{width:'90%',minHeight:'20%',height:'auto',margin:'5%',marginTop:'-4%',marginBottom:'30%'}}>
+        
+        <Paper elevation={5} style={{width:'90%',height:'auto',margin:'5%'}}>
           <Row>
-            <Col>
+            <Col sm={5}>
                 <Skeleton style={{margin:'5%',width:'90%',height:'95%',marginTop:'-1%'}} />
             </Col>
-            <Col>
+            <Col sm={7}>
               <Card>
-                <CardContent >
+                <CardContent>
                   <Typography style={{fontFamily:"csPrajad" ,fontSize:18}}>
                     สถิติของสถานที่ท่องเที่ยว
                   </Typography>
@@ -349,13 +342,40 @@ export class MapContainer extends React.Component  {
                   </Typography>
                   <br />
                   <Divider/>
+
                   <Typography style={{fontFamily:"csPrajad",fontSize:16}}>
                     <br />
                     <LoyaltyIcon />
-                    ความนิยม : 
+                    ความนิยม 
                   </Typography>
+                  <List style={{marginLeft:'5%'}}>
+                     <ListItem>
+                        <Typography style={{fontFamily:"csPrajad",fontSize:16}}>
+                        <CommuteIcon />
+                        Popularity : 
+                      </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography style={{fontFamily:"csPrajad",fontSize:16}}>
+                        <TodayIcon />
+                        Day-of-Week : 
+                      </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography style={{fontFamily:"csPrajad",fontSize:16}}>
+                        <DateRangeIcon />
+                        Favourite Month :
+                      </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Typography style={{fontFamily:"csPrajad",fontSize:16}}>
+                        <AccessTimeIcon />
+                        Time Ranges :
+                      </Typography>
+                    </ListItem>
+                  </List>
+
                   <br />
-                  <Divider/>
                 </CardContent>
               </Card>
 
