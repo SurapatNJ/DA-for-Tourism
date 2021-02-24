d413c3c9cc887bae6ee2bb4955a0c6d9fd186332
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2021-02-24 17:47:49 +0700

    Update Login.js

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,5 +1,5 @@
   
-import React from 'react';
+import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -18,17 +18,18 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import VpnKeyOutlinedIcon from '@material-ui/icons/VpnKeyOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import GoogleLogin from 'react-google-login';
 
+import axios from 'axios';
 
 
 const responseGoogle = (response) => {
   console.log(response);
 }
 
-const useStyles = makeStyles((theme) => ({
+const style = (theme) => ({
   paper: {
     display: 'flex',
     flexDirection: 'column',
@@ -55,103 +56,173 @@ const useStyles = makeStyles((theme) => ({
     form: {
       width: '80%', // Fix IE 11 issue.
       marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(3),
+      margin: theme.spacing(3),
     },
 
-  }));
+  });
+    
+
+export class LogIn extends React.Component{
+
+    state = {
+      username:"",
+      email:"",
+      password:""
+    }
+
+
+    componentDidMount() { 
+      let data; 
+      axios 
+          .get("http://104.248.7.194:8000/api/login/") 
+          .then((res) => { 
+              data = res.data; 
+              this.setState({ 
+                  text: data, 
+              }); 
+              console.log(res.statusText);
+          }) 
+          .catch((err) => {}); 
+  } 
+
+
+    handleUsername = (e) => { 
+        this.setState({ 
+            username : e.target.value, 
+        }); 
+    }; 
+
+    handlePassword = (e) => { 
+      this.setState({ 
+          password: e.target.value, 
+      }); 
+  }; 
+
+    handleSubmit = (e) => { 
+        e.preventDefault(); 
+        console.log("submitted");
+        console.log(this.state)
+        axios 
+        
+            .post("http://104.248.7.194:8000/api/login/", { 
+              username: this.state.username, 
+              email: this.state.email,
+              password: this.state.password 
+            },{  
+              headers: {
+              'Content-Type': 'application/json'
+            }}) 
+            .then((res) => { 
+              this.setState({ 
+                username: "",
+                email:"",
+                password: "", 
+            });
+            console.log(res);
+            console.log(res.data);
+            }) 
+            .catch((err) => {console.log(err)}); 
+    }; 
   
+  render(){
+    const { classes } = this.props;
+      return (
+        <div className="login">
+        <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossorigin="anonymous"
+        />
 
 
-export default function LogIn() {
-    const classes = useStyles();
+        <section className="App-section">
+        <Container fluid>
+            <Row>
+              <Paper elevation={5} style={{width:'50%',height:'50%',margin:'8.5%',marginLeft:'25%'}} className={classes.paper}>
+                <Card className={classes.card}>
+                <CardContent style={{width:'100%'}} className={classes.cardcontent}>
+                  <Avatar className={classes.avatar}>
+                  <VpnKeyOutlinedIcon />
+                  </Avatar>
+                  <Typography variant="h5" style={{fontFamily:'csPrajad',fontWeight:'bold'}}>
+                    Log in
+                  </Typography>
 
-  return (
-    <div classname="login">
-    <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous"
-    />
+                  <Divider style={{width:'90%',marginTop:'3%'}}/>
 
+                  <form noValidate className={classes.form} >
+                    <Grid container>
+                      <Grid item xs={12}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                        onChange={this.handleUsername}
+                      />
+                      </Grid>
+                      <Grid item xs={12}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={this.handlePassword}
+                      />
+                      </Grid>
+                      <Grid item xs={12}>
+                      <Button
+                        type="submit"
+                        fullWidth
+                        style={{marginTop:'2%'}}
+                        variant="contained"
+                        color="secondary"
+                        onClick={this.handleSubmit}
+                      >
+                        Sign In
+                      </Button>
+                      </Grid>
+                        <Grid item xs={7}>
+                        </Grid>
+                        <Grid item xs={5}>
+                          <Link href="/pages/SignUp" variant="body2">
+                            {"Sign Up a new Account"}
+                          </Link>
+                        </Grid>
+                      </Grid>
+              </form>
+              {/*                <GoogleLogin
+                    // change client id
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Login with Google Account"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                      />
+                      */}
 
-    <section className="App-section">
-    <Container fluid>
-        <Row>
-          <Paper elevation={5} style={{width:'50%',height:'50%',margin:'8.5%',marginLeft:'25%'}} className={classes.paper}>
-            <Card className={classes.card}>
-            <CardContent style={{width:'100%'}} className={classes.cardcontent}>
-              <Avatar className={classes.avatar}>
-              <VpnKeyOutlinedIcon />
-              </Avatar>
-              <Typography variant="h5" style={{fontFamily:'csPrajad',fontWeight:'bold'}}>
-                Log in
-              </Typography>
-              <Divider style={{width:'90%',marginTop:'3%'}}/>
-              <form className={classes.form} noValidate>
-              <Grid container>
-                <Grid item xs={12}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-          />
-          </Grid>
-          <Grid item xs={12}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          </Grid>
-          <Grid item xs={12}>
-          <Button
-            type="submit"
-            fullWidth
-            style={{marginTop:'2%'}}
-            variant="contained"
-            color="secondary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          </Grid>
-            <Grid item xs={7}>
-            </Grid>
-            <Grid item xs={5}>
-              <Link href="/pages/SignUp" variant="body2">
-                {"Sign Up a new Account"}
-              </Link>
-            </Grid>
-          </Grid>
-          </form>
-                <GoogleLogin
-                // change client id
-                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                    buttonText="Login with Google Account"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                  />
-            </CardContent>
-            </Card>
-          </Paper>
-        </Row>
-    </Container>
-    </section>
-    </div>
+                </CardContent>
+                </Card>
+              </Paper>
+            </Row>
+        </Container>
+        </section>
+        </div>
     
   );
 }
+}
+
+
+export default withStyles(style)(LogIn);
